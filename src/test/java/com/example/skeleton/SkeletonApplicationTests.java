283eee585ba3151b16c692dcfc095dbd08b8cd44
Yoojin Lee <yoojinLee.dev@gmail.com>
2023-10-31 11:29:20 +0900

    Test: IntegrationTest 추가

--- src/test/java/com/example/skeleton/SkeletonApplicationTests.java
+++ src/test/java/com/example/skeleton/SkeletonApplicationTests.java
@@ -2,7 +2,9 @@ package com.example.skeleton;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootTest
 class SkeletonApplicationTests {
 
